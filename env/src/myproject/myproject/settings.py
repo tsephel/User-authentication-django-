b3a4d566b564f61f421f652042c215f47304aee9
tsephel <ngawangkunga01@gmail.com>
2020-02-27 19:30:45 +0545

    Error Fixed

--- env/src/myproject/myproject/settings.py
+++ env/src/myproject/myproject/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'crispy_forms',
-    'client',
 ]
 
 MIDDLEWARE = [
--- env/src/myproject/myproject/urls.py
+++ env/src/myproject/myproject/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('client/', include('client.urls'))
+    
 
 ]
 
